3bf7f4c41ed7c4d95bf35cfb06f5df0197b4cf23
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2023-04-07 13:07:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports={
     images:{
-        domains:['links.papareact.com','d16pnh712pyiwa.cloudfront.net','www.citypng.com','fakestoreapi.com','unsplash.com','th.bing.com','i.ytimg.com','data1.ibtimes.co.in','lh3.googleusercontent.com','api.escuelajs.co']
+        domains:['links.papareact.com','logos-world.net','d16pnh712pyiwa.cloudfront.net','www.citypng.com','fakestoreapi.com','unsplash.com','th.bing.com','i.ytimg.com','data1.ibtimes.co.in','lh3.googleusercontent.com','api.escuelajs.co']
     },
     env:{
         stripe_public_key:process.env.STRIPE_PUBLIC_KEY
