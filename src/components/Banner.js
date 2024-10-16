ac278dd5b6df16c3a6afd072861d4c27dbbd809f
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2024-10-16 12:21:56 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,10 +9,15 @@ function Banner() {
       <Carousel
       autoPlay
       infiniteLoop
-      showStatu
+      showStatus={false}
+      showIndicators={false}
+      showThumbs={false}
       interval={1000}
       >
-        <div><img loading='lazy' src="https://www.sh/media/0610_HP_09_RTW_BikeShorts_1559243426341.jpg?scl=1&wid=1440" alt="" /></div>
+        <div><img loading='lazy' src="https://www.shutterstock.com/image-vector/3d-yellow-great-discount-sale-260nw-2056851839.jpg" alt="" /></div>
+        <div><img loading='lazy' src="https://th.bing.com/th/id/R.497762a8e92f3c9c065232a70fc43b18?rik=FnPAPfsDv7eGXQ&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0217%2f6856%2ffiles%2f1080x608-1_1024x1024.jpg%3f50257&ehk=fvfzgUDmVSpilNam5HNhENNUdrPckHG9MMLbiZXQlv0%3d&risl=&pid=ImgRaw&r=0" alt="" /></div>
+        <div><img loading='lazy' src="https://i.ytimg.com/vi/tYpCyI89Yf8/maxresdefault.jpg" alt="" /></div>
+        <div><img loading='lazy' src="https://images.bloomingdalesassets.com/is/image/BcomMedia/media/0610_HP_09_RTW_BikeShorts_1559243426341.jpg?scl=1&wid=1440" alt="" /></div>
         <div><img loading='lazy' src="https://i.pinimg.com/originals/8e/dc/a0/8edca0fc9ba10f24a23a34753ec53588.jpg" alt="" /></div>
 
       </Carousel>
