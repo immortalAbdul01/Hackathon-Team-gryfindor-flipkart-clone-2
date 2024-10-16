90428affc3f6e61858f3ed8db16d5503005cfcb5
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2024-10-16 12:20:29 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,8 +12,7 @@ function Banner() {
       showStatu
       interval={1000}
       >
-        <div><img loading='lazy' src="https://www.shutterstock.com/image-vector/3d-yellow-great-discount-sale-260nw-2056851839.jpg" alt="" /></div>
-        <div><img loading='lazy' src="https://th.com/is/image/BcomMedia/media/0610_HP_09_RTW_BikeShorts_1559243426341.jpg?scl=1&wid=1440" alt="" /></div>
+        <div><img loading='lazy' src="https://www.sh/media/0610_HP_09_RTW_BikeShorts_1559243426341.jpg?scl=1&wid=1440" alt="" /></div>
         <div><img loading='lazy' src="https://i.pinimg.com/originals/8e/dc/a0/8edca0fc9ba10f24a23a34753ec53588.jpg" alt="" /></div>
 
       </Carousel>
